6fdf01f8f39b8152d150db96a997f323eac7f9dc
Prabhu Kumar Ponnaganti <prabhukumarp@gmail.com>
2025-05-01 01:29:22 +0530

    Fixed image in extent report

--- src/test/java/com/prabhu/myapp/listeners/TestListener.java
+++ src/test/java/com/prabhu/myapp/listeners/TestListener.java
@@ -17,6 +17,7 @@ import org.testng.ITestResult;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Base64;
 
 public class TestListener implements ITestListener {
 
@@ -46,13 +47,42 @@ public class TestListener implements ITestListener {
         logger.info("✅ Test Passed: {}", testName);
     }
 
-    @Override
+    /*@Override
     public void onTestFailure(ITestResult result) {
         String testName = result.getMethod().getMethodName();
         ExtentTestManager.getTest().fail("❌ Test Failed");
         captureScreenshot(result);
         logger.error("❌ Test Failed: {}", testName);
+    }*/
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        Object currentInstance = result.getInstance();
+
+        if (currentInstance instanceof BaseTest baseTest) {
+            Page page = baseTest.getPage();
+            String testName = result.getMethod().getMethodName();
+            captureScreenshot(result);
+            if (page != null) {
+                try {
+                    byte[] screenshotBytes = page.screenshot(new Page.ScreenshotOptions());
+                    String base64Screenshot = Base64.getEncoder().encodeToString(screenshotBytes);
+
+                    ExtentTestManager.getTest().fail("❌ Test Failed. Screenshot below:")
+                            .addScreenCaptureFromBase64String(base64Screenshot, testName + "_screenshot");
+
+                    logger.error("❌ Test Failed: {}", testName);
+                } catch (Exception e) {
+                    logger.error("Failed to capture Base64 screenshot", e);
+                }
+            } else {
+                ExtentTestManager.getTest().fail("❌ Test Failed. Page object was null, screenshot not captured.");
+                logger.error("Unable to Capture Screenshot as page is null");
+            }
+        }
     }
+
+
 
     @Override
     public void onFinish(ITestContext context) {
@@ -71,7 +101,7 @@ public class TestListener implements ITestListener {
 
                 FileHelper.ensureDirectoryExists(screenshotPath);
                 page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(screenshotFile)));
-                ExtentTestManager.getTest().addScreenCaptureFromPath(FileHelper.toWebPath(screenshotFile));
+                //ExtentTestManager.getTest().addScreenCaptureFromPath(FileHelper.toWebPath(screenshotFile));
             } else {
                 logger.warn("📸 Could not capture screenshot. Page is null.");
             }
